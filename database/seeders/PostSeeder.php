69e96fd6a23d34cef05fc4c4e3adb6b0328e6c23
devmanuelsharmaine <50508865+devmanuelsharmaine@users.noreply.github.com>
2022-03-02 15:45:17 +0800

    database and seeder

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        DB::table('posts')->insert([
+            'title' => 'Post Sample',
+            'description' => 'Sample Description',
+            'photo' => 'test',
+            'user_id' => 1,
+            'created_at' => Carbon::now()
+        ]);
+
+    }
+}
